3de953fffd2cf05828417bda08fbdc7dc28461b9
unknown <automationt986@gmail.com>
2026-01-29 01:14:52 +0530

    changes

--- src/main/java/LibraryFiles/BaseClass.java
+++ src/main/java/LibraryFiles/BaseClass.java
@@ -4,44 +4,135 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
 
-public class BaseClass
-{
-	public WebDriver driver;
-	public void initializeBrowser() throws IOException
-	{
+public class BaseClass {
 
-	    ChromeOptions options = new ChromeOptions();
+	protected WebDriver driver;
 
-	    Map<String, Object> prefs = new HashMap<>();
-	    prefs.put("credentials_enable_service", false);
-	    prefs.put("profile.password_manager_enabled", false);
-	    prefs.put("profile.password_manager_leak_detection", false);
+	public void initializeBrowser() throws IOException {
 
-	    options.setExperimentalOption("prefs", prefs);
+		        String browser = System.getProperty("Browser", "chrome").toLowerCase();
 
-	    options.addArguments(
-	        "--disable-features=PasswordManagerEnableCheck,PasswordLeakDetection,PasswordProtectionWarningTrigger"
-	    );
+		        switch (browser) {
+		        case "chrome":
+		            driver = new ChromeDriver(getChromeOptions());
+		            break;
 
-	    options.addArguments("--disable-notifications");
-	    options.addArguments("--disable-infobars");
+		        case "edge":
+		            driver = new EdgeDriver(getEdgeOptions());
+		            break;
 
-	    // 🔥 THIS IS CRITICAL
-	    options.addArguments(
-	        "--user-data-dir=" + System.getProperty("java.io.tmpdir") + "/selenium-profile"
-	    );
+		        case "firefox":
+		            driver = new FirefoxDriver(getFirefoxOptions());
+		            break;
 
-		driver=new ChromeDriver(options);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		driver.manage().window().maximize();
-		driver.get(UtilityClass.getTestDataFromPropertyFiles("URL"));
+		        default:
+		            throw new IllegalArgumentException("Unsupported browser: " + browser);
+		        }
+
+		        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		        driver.manage().window().maximize();
+		        driver.get(UtilityClass.getTestDataFromPropertyFiles("URL"));
+		    }
+
+		    // ================= CHROME =================
+		    private ChromeOptions getChromeOptions() {
+		        ChromeOptions options = new ChromeOptions();
+
+		        Map<String, Object> prefs = new HashMap<>();
+		        prefs.put("credentials_enable_service", false);
+		        prefs.put("profile.password_manager_enabled", false);
+		        prefs.put("profile.default_content_setting_values.notifications", 2);
+
+		        options.setExperimentalOption("prefs", prefs);
+
+		        options.addArguments(
+		                "--disable-notifications",
+		                "--disable-infobars",
+		                "--disable-extensions",
+		                "--disable-popup-blocking",
+		                "--disable-features=PasswordManagerEnableCheck,PasswordLeakDetection",
+		                "--remote-allow-origins=*"
+		        );
+
+		        return options;
+		    }
+
+		    // ================= EDGE =================
+		    private EdgeOptions getEdgeOptions() {
+		        EdgeOptions options = new EdgeOptions();
+
+		        Map<String, Object> prefs = new HashMap<>();
+		        prefs.put("credentials_enable_service", false);
+		        prefs.put("profile.password_manager_enabled", false);
+		        prefs.put("profile.default_content_setting_values.notifications", 2);
+
+		        options.setExperimentalOption("prefs", prefs);
+
+		        options.addArguments(
+		                "--disable-notifications",
+		                "--disable-infobars",
+		                "--disable-extensions",
+		                "--disable-popup-blocking"
+		        );
+
+		        return options;
+		    }
+
+		    // ================= FIREFOX =================
+		    private FirefoxOptions getFirefoxOptions() {
+		        FirefoxOptions options = new FirefoxOptions();
+
+		        options.addPreference("dom.webnotifications.enabled", false);
+		        options.addPreference("dom.push.enabled", false);
+		        options.addPreference("signon.rememberSignons", false);
+		        options.addPreference("browser.download.panel.shown", false);
+
+		        return options;
+		    }
+
+		    @AfterClass
+		    public void closeBrowser() {
+		        if (driver != null) {
+		            driver.quit();
+		        }
+		    }
 		
+
+//
+//	    ChromeOptions options = new ChromeOptions();
+//
+//	    Map<String, Object> prefs = new HashMap<>();
+//	    prefs.put("credentials_enable_service", false);
+//	    prefs.put("profile.password_manager_enabled", false);
+//	    prefs.put("profile.password_manager_leak_detection", false);
+//
+//	    options.setExperimentalOption("prefs", prefs);
+//	    options.addArguments(
+//	        "--disable-features=PasswordManagerEnableCheck,PasswordLeakDetection,PasswordProtectionWarningTrigger");
+//
+//	    options.addArguments("--disable-notifications");
+//	    options.addArguments("--disable-infobars");
+//
+//	    // 🔥 THIS IS CRITICAL
+//	    options.addArguments(
+//	        "--user-data-dir=" + System.getProperty("java.io.tmpdir") + "/selenium-profile"
+//	    );
+//
+//		driver=new ChromeDriver(options);
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+//		driver.manage().window().maximize();
+//		driver.get(UtilityClass.getTestDataFromPropertyFiles("URL"));
+//		
 	}
 
-}
+
